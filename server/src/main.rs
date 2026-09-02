c54f84424bb8952bb1d03707af5e6884f2a57bc0
prushton2 <peterrushton418@gmail.com>
2026-09-02 14:54:22 -0400

    Added iroh

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,16 @@
 use enigo::Mouse;
 use enigo::{Button, Coordinate::Abs, Direction::Click, Enigo, Settings};
+use iroh::endpoint::presets::{self, Preset};
+use iroh::{Endpoint, EndpointAddr, PublicKey};
 
-fn main() {
-    let mut enigo = Enigo::new(&Settings::default()).unwrap();
+#[tokio::main]
+async fn main() {
+    // let mut enigo = Enigo::new(&Settings::default()).unwrap();
     
-    enigo.move_mouse(1280, 800, Abs).unwrap();
-    enigo.button(Button::Left, Click).unwrap();
+    // enigo.move_mouse(1280, 800, Abs).unwrap();
+    // enigo.button(Button::Left, Click).unwrap();
+
+    let addr: EndpointAddr = EndpointAddr::new(PublicKey::from_bytes(&[0; 32]).unwrap());
+    let ep = Endpoint::bind(presets::N0).await.unwrap();
+    let conn = ep.connect(addr, b"my-alpn").await.unwrap();
 }
