693158a7993fe68a3af65cfe1ed1c2bd38077719
prushton2 <peterrushton418@gmail.com>
2026-09-04 10:16:23 -0400

    Moving server logic into iced

--- server/src/main.rs
+++ server/src/main.rs
@@ -15,44 +15,36 @@ async fn main() {
     // let mut mouse = mouse::enigo::EnigoMouse::new();
     let mut mouse = mouse::dummy::DummyMouse::new();
 
-    let (server, key) = p2p::P2P::init().await.unwrap();
-    let pin = p2p::remote_key_store::generate_key();
-    let key = key.to_string();
-    p2p::remote_key_store::set(&pin, &key.to_string()).await;
 
     let server_info = protocol::ServerInformation {
         width: 1920,
         height: 1080
     };
 
-    let p2p_arc = Arc::new(Mutex::new(server));
-    let p2p_arc_clone = Arc::clone(&p2p_arc);
-
     let _ = iced::application(
         move || {
-            let arc: Arc<Mutex<p2p::P2P>> = p2p_arc_clone.clone();
-            window::Window::boot(server_info, &pin, &key, arc)
+            window::Window::boot(server_info)
         },
         window::Window::update,
         window::Window::view
     )
         .run();
 
-    let mut server_lock = p2p_arc.lock().await;
+    // let mut server_lock = p2p_arc.lock().await;
 
-    let _ = server_lock.send(&server_info.into_bytes()).await;
+    // let _ = server_lock.send(&server_info.into_bytes()).await;
 
-    loop {
-        let response = server_lock.read().await.unwrap();
-        let parsed = p2p::protocol::FromBytes::parse(&response);
-        match parsed {
-            FromBytes::MouseMove(message) => {
-                mouse.move_mouse(message.x, message.y);
-            }
-            FromBytes::MouseClick(message) => {
-                mouse.click_mouse(message.button, message.state);
-            }
-            _ => {}
-        }
-    }
+    // loop {
+    //     let response = server_lock.read().await.unwrap();
+    //     let parsed = p2p::protocol::FromBytes::parse(&response);
+    //     match parsed {
+    //         FromBytes::MouseMove(message) => {
+    //             mouse.move_mouse(message.x, message.y);
+    //         }
+    //         FromBytes::MouseClick(message) => {
+    //             mouse.click_mouse(message.button, message.state);
+    //         }
+    //         _ => {}
+    //     }
+    // }
 }
--- server/src/window.rs
+++ server/src/window.rs
@@ -1,3 +1,4 @@
+use p2p::p2p::P2PError;
 use tokio::sync::Mutex;
 use std::sync::Arc;
 
@@ -8,28 +9,30 @@ use p2p::protocol::ServerInformation;
 
 pub struct Window {
     server_info: ServerInformation,
-    pin: String,
-    key: String,
-    p2p: Arc<Mutex<p2p::P2P>>,
-    waiting: bool,
+    pin: Option<String>,
+    key: Option<String>,
+    p2p: Option<Arc<Mutex<p2p::P2P>>>,
+    wait_reason: String,
+    error: String,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub enum Message {
-    StartWait,
+    Register,
+    AwaitClient(Result<(Arc<Mutex<p2p::P2P>>, String, String), Arc<P2PError>>),
     ConnectionEstablished(Result<(), String>),
-
     Drop
 }
 
 impl Window {
-    pub fn boot(server_information: ServerInformation, pin: &String, key: &String, p2p: Arc<Mutex<p2p::P2P>>) -> Self {
+    pub fn boot(server_information: ServerInformation) -> Self {
         let this = Self {
             server_info: server_information,
-            pin: pin.clone(),
-            key: key.clone(),
-            p2p: p2p,
-            waiting: false,
+            pin: None,
+            key: None,
+            p2p: None,
+            wait_reason: String::from(""),
+            error: String::from("")
         };
 
         this
@@ -37,10 +40,40 @@ impl Window {
 
     pub fn update(&mut self, message: Message) -> Task<Message>{
         match message {
-            Message::StartWait => {
-                self.waiting = true;
-                let reference = self.p2p.clone();
+            Message::Register => {
+                self.wait_reason = "Registering...".to_owned();
+
+                Task::perform(
+                    async {
+                        let (server, key) = p2p::P2P::init().await.map_err(|e| Arc::new(e))?;
+
+                        let pin = p2p::remote_key_store::generate_key();
+                        let key = key.to_string();
+                        p2p::remote_key_store::set(&pin, &key.to_string()).await;
+
+                        Ok((Arc::new(Mutex::new(server)), key, pin))
+                    },
+                    Message::AwaitClient
+                )
+            },
+            Message::AwaitClient(result) => {
+                let (p2p, key, pin) = match result {
+                    Ok(t) => t,
+                    Err(t) => {
+                        self.error = format!("{:?}", t);
+                        self.wait_reason = String::from("");
+                        return Task::none()
+                    }
+                };
+
+                self.p2p = Some(p2p);
+                self.key = Some(key);
+                self.pin = Some(pin);
+
+                let arc = self.p2p.as_mut().unwrap();
+                let reference = arc.clone();
                 
+                self.wait_reason = "Waiting for connection".to_owned();
                 Task::perform(
                 async move {
                         let mut lock = reference.lock().await;
@@ -62,14 +95,16 @@ impl Window {
     }
 
     pub fn view(&self) -> iced::Element<'_, Message> {
-        let pin = format!("Pin: {}", self.pin);
-        let key = format!("Key: {}", self.key);
+        let pin = format!("Pin: {:?}", self.pin);
+        let key = format!("Key: {:?}", self.key);
         column![
+            text(&self.error),
             text_input(&pin, &pin).on_input(|_| Message::Drop),
             text_input(&key, &key).on_input(|_| Message::Drop),
             text(format!("Sys Info: {:?}", self.server_info)),
-            button("Wait for client").on_press(Message::StartWait),
-            text(if self.waiting { "Waiting for client to connect..." } else { "" }),
+            button("Allow Connections").on_press(Message::Register),
+            text(&self.wait_reason),
+            text(&self.error),
         ]
         .spacing(10)
         .padding(20)
